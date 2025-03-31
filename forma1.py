53f693e4ac91181ad77ddc2c06efa6d060816d03
Szabolcs Juhász <juhasz.szabolcs@szbi-pg.hu>
2025-03-31 07:33:46 +0200

    init project

---
+++ forma1.py
@@ -0,0 +1,16 @@
+"""
+Olvasd be az f1.txt adatait, majd oldd meg az alábbi feladatokat!
+
+1. Hány versenyző szerepel a fájlban?
+2. Melyik versenyző nyerte a legtöbb futamot?
+3. Ki teljesített a legtöbb futamot?
+4. Átlagosan hány futamot teljesítettek a versenyzők?"
+
+A megoldott feladatokat a kiirt_adatok nevű mappába hozd létre statisztika.txt néven!
+"""
+
+
+print("A beolvasott fájlban összesen ____ versenyző szerepel.")
+print("A legtöbb futamot nyert versenyző: ____")
+print("A legtöbb futamot teljesített versenyző: ____")
+print("Az átlagos futamszám: ____")
